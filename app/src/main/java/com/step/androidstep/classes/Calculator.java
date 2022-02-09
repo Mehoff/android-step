0ef9e3bf7ff5ac26c98ed56b72c883564dff24ea
Mehoff <mixov2000@gmail.com>
2022-02-09 23:25:15 +0200

    Equals demo

--- app/src/main/java/com/step/androidstep/classes/Calculator.java
+++ app/src/main/java/com/step/androidstep/classes/Calculator.java
@@ -1,6 +1,7 @@
 package com.step.androidstep.classes;
 
 import android.os.Debug;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ import com.step.androidstep.statics.ArrayHelper;
 import java.util.ArrayList;
 
 public class Calculator {
+    private final String TAG = "Calc";
     private final int MAX_NUMBER_SIZE = 9;
     private TextView calcExpressionView;
     private TextView calcResultView;
@@ -105,12 +107,100 @@ public class Calculator {
         }
     }
 
-    private void clear(){
+    private Action getActionFromChar(char c){
+        switch (c){
+            case '+': return Action.PLUS;
+            case '-': return Action.MINUS;
+            case '/': return  Action.DIVIDE;
+            case '*': return  Action.MULTIPLY;
+            case '.': return Action.ASTERISK;
+            default: return null;
+        }
+    }
 
+    private void clear(){
+        expression = "0";
+        actionHistory.clear();
     }
 
     private void equals(){
+//        if(!isWritable( actionHistory.get(actionHistory.size() - 2) )){
+//            Log.i(TAG, "Последний экшн не число, ничего не делаем");
+//            return;
+//        }
 
+        boolean isFirstArg = true;
+        Action action = Action.EQUALS;
+        String arg1_str = "";
+        String arg2_str = "";
+        for(int i = 0; i < expression.length(); i++){
+            boolean isDigit = Character.isDigit(expression.charAt(i));
+
+            // todo: ignore "."
+            if(isDigit){
+                if(isFirstArg){
+                    arg1_str += expression.charAt(i);
+                    Log.i(TAG, "arg1_str + " + expression.charAt(i));
+                } else {
+                    Log.i(TAG, "arg2_str + " + expression.charAt(i));
+                    arg2_str += expression.charAt(i);
+                }
+            } else {
+                if(!isFirstArg){
+                    // Second arg filled, make calculation and put it in first arg
+                    // if expression string is not ended
+
+                    int arg1 = Integer.parseInt(arg1_str);
+                    int arg2 = Integer.parseInt(arg2_str);
+
+                    Log.i(TAG, "arg1 = " + arg1);
+                    Log.i(TAG, "arg2 = " + arg2);
+
+
+                    int result = calculate(arg1, arg2, action);
+                    Log.i(TAG, "result = " + result);
+
+
+                    // End of expression - print result
+                    if(i == expression.length() - 1){
+                        calcResultView.setText(result + "");
+                        calcExpressionView.setText("0");
+                    }
+                    // Still have arg to parse
+                    else {
+                        arg1_str = String.valueOf(result);
+                        arg2_str = "";
+                        action = Action.EQUALS;
+                    }
+                } else {
+                    // . + * - /
+                    action = getActionFromChar(expression.charAt(i));
+                    Log.i(TAG, "Action = " + action);
+
+                    if(action == null){
+                        return;
+                        //throw new Exception("Undefined action exception");
+                    }
+                    isFirstArg = false;
+                }
+
+            }
+        }
+
+        int arg1 = Integer.parseInt(arg1_str);
+        int arg2 = Integer.parseInt(arg2_str);
+
+        Log.i(TAG, "arg1 = " + arg1);
+        Log.i(TAG, "arg2 = " + arg2);
+
+
+        int result = calculate(arg1, arg2, action);
+        Log.i(TAG, "result = " + result);
+
+
+        calcResultView.setText(result + "");
+        expression = result + "";
+        updateCalcExpressionUI();
     }
 
     private void erase(){
@@ -121,5 +211,24 @@ public class Calculator {
 
         expression = expression.substring(0, expression.length() - 1);
         updateCalcExpressionUI();
+    }
+
+    private int calculate(int a, int b, Action action){
+        switch (action) {
+            case PLUS: return a + b;
+            case MINUS: return a - b;
+            case DIVIDE: return divide(a, b);
+            case MULTIPLY: return a * b;
+            default: return -1;
+
+            //throw unexpected action exception
+        }
+    }
+
+    private int divide(int a, int b){
+        if(b == 0){
+            // throw ZeroDivisionException
+        }
+        return a / b;
     }
 }
